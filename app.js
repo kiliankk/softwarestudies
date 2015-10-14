20c73dfd6a02581fd920afb03c8d1334bce2281b
kiliankk <kilian@dhcp-86-37.eduroam.aalto.fi>
2015-10-14 11:35:09 +0300

    my function
    I haven’t figured out how to select a value for food when calling the
    function…

---
+++ app.js
@@ -0,0 +1,18 @@
+// console.log('hello world')
+//array or object and flow control 
+
+var food = ['chips','salat']
+
+function myfood (food[1]) 
+{
+	if (food[0] === 'chips') 
+	{
+		console.log('I have ' + food[0])
+	} 
+	else if (food[1] === 'salat') 
+	{
+		console.log('I have ' + food[1])
+	}
+}
+
+myfood()
